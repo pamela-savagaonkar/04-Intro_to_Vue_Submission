3f61382817c9128e37811ac0eb46a69acf141538
pamela-savagaonkar <159180052+pamela-savagaonkar@users.noreply.github.com>
2024-10-10 12:52:43 -0700

    updated public path for github page

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,9 @@
 // vue.config.js
 module.exports = {
-  publicPath: './', // Use relative paths for serving files
+  publicPath: '/04-Intro_to_Vue_Submission/', // Set this to your repo name
   outputDir: 'dist', // Output directory for build
   assetsDir: 'assets', // Directory for static assets
   devServer: {
-    // Server configuration
     host: 'localhost',
     port: 8080,
   },
